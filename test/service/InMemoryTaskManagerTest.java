c4cf07ab2076249ffd1dadb6ed2798ef335519e9
Anatolii <Steg92sm13@yandex.ru>
2024-04-11 21:43:43 +0800

    Дописал немного тестов

--- test/service/InMemoryTaskManagerTest.java
+++ test/service/InMemoryTaskManagerTest.java
@@ -122,4 +122,75 @@ class InMemoryTaskManagerTest {
         assertEquals(1, subtasks.size(), "Неверное количество задач.");
         assertEquals(subtask, subtasks.get(0), "Задачи не совпадают.");
     }
+
+    @Test
+    void deleteId() {
+        //setup
+        Managers.setTaskManager(null);
+        TaskManager taskManager = Managers.getDefault();
+        Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
+
+        //act
+        taskManager.createTask(task);
+
+        //verify
+        taskManager.delete(0L);
+        final List<Task> tasks = taskManager.getAllTasks();
+        assertEquals(0, tasks.size(), "Неверное количество задач.");
+    }
+
+    @Test
+    void deleteAllTask() {
+        //setup
+        Managers.setTaskManager(null);
+        TaskManager taskManager = Managers.getDefault();
+        Task task0 = new Task("Test addNewTask", "Test addNewTask description", NEW);
+        Task task1 = new Task("Test addNewTask", "Test addNewTask description", NEW);
+
+        //act
+        taskManager.createTask(task0);
+        taskManager.createTask(task1);
+
+        //verify
+        taskManager.deleteAllTasks();
+        final List<Task> tasks = taskManager.getAllTasks();
+        assertEquals(0, tasks.size(), "Неверное количество задач.");
+    }
+
+    @Test
+    void deleteAllEpic() {
+        //setup
+        Managers.setTaskManager(null);
+        TaskManager taskManager = Managers.getDefault();
+        Epic epic0 = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
+        Epic epic1 = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
+
+        //act
+        taskManager.createEpic(epic0);
+        taskManager.createEpic(epic1);
+
+        //verify
+        taskManager.deleteAllEpic();
+        final List<Epic> epics = taskManager.getAllEpic();
+        assertEquals(0, epics.size(), "Неверное количество задач.");
+    }
+
+    @Test
+    void deleteAllSubtask() {
+        //setup
+        Managers.setTaskManager(null);
+        TaskManager taskManager = Managers.getDefault();
+        Epic epic = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
+        Subtask subtask0 = new Subtask("Test addNewSubtask", "Test addNewEpic description", NEW, epic);
+        Subtask subtask1 = new Subtask("Test addNewSubtask", "Test addNewEpic description", NEW, epic);
+
+        //act
+        taskManager.createSubtask(subtask0);
+        taskManager.createSubtask(subtask1);
+
+        //verify
+        taskManager.deleteAllSubtask();
+        final List<Subtask> subtasks = taskManager.getAllSubtask();
+        assertEquals(0, subtasks.size(), "Неверное количество задач.");
+    }
 }
