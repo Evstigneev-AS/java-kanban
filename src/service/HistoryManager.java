18e2b99eb3d76cbfc2b073176b44426639d0c9b2
Anatolii <Steg92sm13@yandex.ru>
2024-04-22 20:08:28 +0800

    Добавил метод remove и заглушку в InMemoryHistoryManager

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface HistoryManager {
     void add(Task task);//должен помечать задачи как просмотренные
 
+    void remove(int id);//добавить метод для удаления из истории!
     List<Task> getHistory();//возвращать их список
 }
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -6,22 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private ArrayList<Task> history = new ArrayList<>();
+    private final ArrayList<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        boolean taskDouble=this.history.contains(task);
-        if (taskDouble){
+        boolean taskDouble = this.history.contains(task);
+        if (taskDouble) {
             this.history.remove(task);
         }
         this.history.add(task);
     }
 
     @Override
+    public void remove(int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public List<Task> getHistory() {
-        List<Task> history = new ArrayList<>();
-        history.addAll(this.history);
-        return history;
+        return new ArrayList<>(this.history);
     }
 
 }
