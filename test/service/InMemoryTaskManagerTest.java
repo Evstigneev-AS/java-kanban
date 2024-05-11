430d71abb14aed9c4d4244e1377c1cd21df5d251
Anatolii <Steg92sm13@yandex.ru>
2024-05-11 08:16:24 +0800

    Дописал комментарий к тестам

--- test/service/InMemoryTaskManagerTest.java
+++ test/service/InMemoryTaskManagerTest.java
@@ -15,56 +15,56 @@ class InMemoryTaskManagerTest {
 
     @Test
     public void createTaskCheckTaskAdded() {
-        // setup
+        // setup - object with which we will work and the input data
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
 
-        // act
+        // act - perform the operation we are testing
         Long taskId = inMemoryTaskManager.createTask(task);
 
-        // verify
+        // verify - statements about the correctness of an action
         Task byId = inMemoryTaskManager.taskById(taskId);
         assertEquals(byId, task);
     }
 
     @Test
     public void createEpicCheckEpicAdded() {
-        // setup
+        // setup - object with which we will work and the input data
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Epic epic = new Epic("Test addNewTask", "Test addNewTask description", NEW);
 
-        // act
+        // act - perform the operation we are testing
         Long epicId = inMemoryTaskManager.createEpic(epic);
 
-        // verify
+        // verify - statements about the correctness of an action
         Task byId = inMemoryTaskManager.taskById(epicId);
         assertEquals(byId, epic);
     }
 
     @Test
     public void createSubtaskCheckSubtaskAdded() {
-        // setup
+        // setup - object with which we will work and the input data
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Epic epic = new Epic("Test addNewTask", "Test addNewTask description", NEW);
         Subtask subtask = new Subtask("Test addNewTask", "Test addNewTask description", NEW, epic);
 
-        // act
+        // act - perform the operation we are testing
         Long subtaskId = inMemoryTaskManager.createSubtask(subtask);
 
-        // verify
+        // verify - statements about the correctness of an action
         Task byId = inMemoryTaskManager.taskById(subtaskId);
         assertEquals(byId, subtask);
     }
 
     @Test
     public void createTaskCheckEveryField() {
-        // setup
+        // setup - object with which we will work and the input data
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
 
-        // act
+        // act - perform the operation we are testing
         Long taskId = inMemoryTaskManager.createTask(new Task(47L, "Test addNewTask", "Test addNewTask description", NEW));
 
-        // verify
+        // verify - statements about the correctness of an action
         Task byId = inMemoryTaskManager.taskById(taskId);
         assertEquals(byId.getId(), 47L);
         assertEquals(byId.getName(), "Test addNewTask");
@@ -74,14 +74,14 @@ class InMemoryTaskManagerTest {
 
     @Test
     void createTaskPutOnListTasksCheckListTasks() {
-        //setup
+        //setup - object with which we will work and the input data
         TaskManager taskManager = Managers.getDefault();
         Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createTask(task);
 
-        //verify
+        //verify - statements about the correctness of an action
         final List<Task> tasks = taskManager.getAllTasks();
         assertNotNull(tasks, "Задачи не возвращаются.");
         assertEquals(1, tasks.size(), "Неверное количество задач.");
@@ -90,15 +90,15 @@ class InMemoryTaskManagerTest {
 
     @Test
     void createEpicPutOnListEpicCheckListEpic() {
-        //setup
+        //setup - object with which we will work and the input data
         Managers.setTaskManager(null);
         TaskManager taskManager = Managers.getDefault();
         Epic epic = new Epic("Test addNewEpic", "Test addNewEpic description", NEW);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createEpic(epic);
 
-        //verify
+        //verify - statements about the correctness of an action
         final List<Epic> epics = taskManager.getAllEpic();
         assertNotNull(epics, "Задачи не возвращаются.");
         assertEquals(1, epics.size(), "Неверное количество задач.");
@@ -107,16 +107,16 @@ class InMemoryTaskManagerTest {
 
     @Test
     void createSubtaskPutOnListSubtaskCheckListSubtask() {
-        //setup
+        //setup - object with which we will work and the input data
         TaskManager taskManager = Managers.getDefault();
         Epic epic = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
         Subtask subtask = new Subtask("Test addNewSubtask", "Test addNewEpic description", NEW, epic);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createEpic(epic);
         taskManager.createSubtask(subtask);
 
-        //verify
+        //verify - statements about the correctness of an action
         final List<Subtask> subtasks = taskManager.getAllSubtask();
         assertNotNull(subtasks, "Задачи не возвращаются.");
         assertEquals(1, subtasks.size(), "Неверное количество задач.");
@@ -125,15 +125,15 @@ class InMemoryTaskManagerTest {
 
     @Test
     void deleteId() {
-        //setup
+        //setup - object with which we will work and the input data
         Managers.setTaskManager(null);
         TaskManager taskManager = Managers.getDefault();
         Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createTask(task);
 
-        //verify
+        //verify - statements about the correctness of an action
         taskManager.delete(0L);
         final List<Task> tasks = taskManager.getAllTasks();
         assertEquals(0, tasks.size(), "Неверное количество задач.");
@@ -141,17 +141,17 @@ class InMemoryTaskManagerTest {
 
     @Test
     void deleteAllTask() {
-        //setup
+        //setup - object with which we will work and the input data
         Managers.setTaskManager(null);
         TaskManager taskManager = Managers.getDefault();
         Task task0 = new Task("Test addNewTask", "Test addNewTask description", NEW);
         Task task1 = new Task("Test addNewTask", "Test addNewTask description", NEW);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createTask(task0);
         taskManager.createTask(task1);
 
-        //verify
+        //verify - statements about the correctness of an action
         taskManager.deleteAllTasks();
         final List<Task> tasks = taskManager.getAllTasks();
         assertEquals(0, tasks.size(), "Неверное количество задач.");
@@ -159,17 +159,17 @@ class InMemoryTaskManagerTest {
 
     @Test
     void deleteAllEpic() {
-        //setup
+        //setup - object with which we will work and the input data
         Managers.setTaskManager(null);
         TaskManager taskManager = Managers.getDefault();
         Epic epic0 = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
         Epic epic1 = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createEpic(epic0);
         taskManager.createEpic(epic1);
 
-        //verify
+        //verify - statements about the correctness of an action
         taskManager.deleteAllEpic();
         final List<Epic> epics = taskManager.getAllEpic();
         assertEquals(0, epics.size(), "Неверное количество задач.");
@@ -177,18 +177,18 @@ class InMemoryTaskManagerTest {
 
     @Test
     void deleteAllSubtask() {
-        //setup
+        //setup - object with which we will work and the input data
         Managers.setTaskManager(null);
         TaskManager taskManager = Managers.getDefault();
         Epic epic = new Epic("Test addNewEpicSubtask", "Test addNewEpic description", NEW);
         Subtask subtask0 = new Subtask("Test addNewSubtask", "Test addNewEpic description", NEW, epic);
         Subtask subtask1 = new Subtask("Test addNewSubtask", "Test addNewEpic description", NEW, epic);
 
-        //act
+        //act - perform the operation we are testing
         taskManager.createSubtask(subtask0);
         taskManager.createSubtask(subtask1);
 
-        //verify
+        //verify - statements about the correctness of an action
         taskManager.deleteAllSubtask();
         final List<Subtask> subtasks = taskManager.getAllSubtask();
         assertEquals(0, subtasks.size(), "Неверное количество задач.");
